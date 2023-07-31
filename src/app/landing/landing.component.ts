e6e167ffa77fd449ba2fac9f9cac03b63bffa910
David <daniel.david.surla@gmail.com>
2023-07-31 07:34:01 +0100

    udpated src/app/landing page components

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class LandingComponent {
 
 }
+
+function quickSwitchIntoCard(cursor) {
+  cursor = 1;
+  let buttons = document.getElementsById("button-circle");
+  let cards = document.getElementsById("staff-profiles-carousel");
+  
+  if (cursor < cards.length)
+    cursor = cards.length;
+  
+  if (cursor > 1)
+    cursor = 1;
+  
+  for (i = 0; i < 2; i++)
+    cards[i].style.display = "none";
+  
+  for (j = 0; j < 2; j++)
+    buttons[i].id = buttons[i].id.replace("\tactive", "");
+  
+  buttons[cursor - 1].id += "\tactive";
+  cards[cursor - 1].style.display = "block";
+}
