20f79fb3c7a8c3cede607160b2134f4c966161bb
David <daniel.david.surla@gmail.com>
2023-07-31 05:10:31 +0100

    first ever initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutsComponent } from './abouts/abouts.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutsComponent,
+    ProfilesComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
